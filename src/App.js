aac1357273a66369e51b59125750f512a671d734
Huzaifa-kamran <huzaifakamranirfani@gmail.com>
2024-08-24 12:38:37 -0700

    update

--- src/App.js
+++ src/App.js
@@ -1,31 +1,32 @@
-
-import './App.css';
-import React, { useState } from 'react';
-import  ReactDOM from 'react';
-import { BrowserRouter, Routes, Route, Router } from "react-router-dom";
-import About from './components/About';
-import Navbar from './components/navbar';
-import TextForm from './components/TextForm';
+import "./App.css";
+import React, { useState } from "react";
+import { BrowserRouter, Routes, Route } from "react-router-dom";
+import About from "./components/About";
+import Navbar from "./components/navbar";
+import TextForm from "./components/TextForm";
 function App() {
   const [mode, setMode] = useState("light");
 
-  const toggleMode = () =>{
-    if(mode === "light"){
-      setMode("dark")
-      document.body.style.backgroundColor = "#000"
-    }else{
-      setMode("light")
-      document.body.style.backgroundColor = "#fff"
+  const toggleMode = () => {
+    if (mode === "light") {
+      setMode("dark");
+      document.body.style.backgroundColor = "#000";
+    } else {
+      setMode("light");
+      document.body.style.backgroundColor = "#fff";
     }
-  }
+  };
   return (
-   <BrowserRouter>
-<Navbar title="Text Utils" mode={mode} toggleMode={toggleMode}/>
-   <Routes>
-    <Route path='/' element= {<TextForm heading="Try TextUtils - word counter, character counter, remove extra spaces" mode={mode}/>} />
-    <Route path='/about' element= {<About mode={mode}/>} />
-</Routes>
-   </BrowserRouter>
+    <BrowserRouter>
+      <Navbar title="Text Utils By Huzaifa" mode={mode} toggleMode={toggleMode} />
+      <Routes>
+        <Route
+          path="/"
+          element={
+            <TextForm heading="Try TextUtils - word counter, character counter, remove extra spaces" mode={mode} />} />
+        <Route path="/about" element={<About mode={mode} />} />
+      </Routes>
+    </BrowserRouter>
   );
 }
 
